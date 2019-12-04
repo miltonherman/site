cbfd51f80034a583f7385b9d49c1b75fc22e8fc1
kukurudz-volodymyr <kukurudz.volodymyr@gmail.com>
2019-12-04 15:50:04 +0200

    refactor

--- src/components/landing/latest-blog/latest-blog.js
+++ src/components/landing/latest-blog/latest-blog.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import LandingButton from '../button/button';
 import './latest-blog.scss';
+import '../landing.scss';
 
 class LatestBlog extends React.Component {
   render() {
     return (
       <div className={'max-width-container center-content latest-blog'}>
         <div className={'latest-blog__row my-4'}>
-          <div className={'latest-blog__title mb-3'}>Latest Roadmap Headline</div>
-          <div className={'latest-blog__description mb-3'}>
+          <div className={'latest-blog__title text-center landing__title mb-3'}>
+            Latest Roadmap Headline
+          </div>
+          <div className={'landing__description text-center mb-3'}>
             This is an impact statement. Something a little bit longer. Maybe mention the specific
             pain point addressed.
           </div>
@@ -17,7 +20,9 @@ class LatestBlog extends React.Component {
           </div>
         </div>
         <div className={'latest-blog__row my-4'}>
-          <div className={'latest-blog__title mb-3'}>Latest Blog Headline</div>
+          <div className={'latest-blog__title text-center landing__title mb-3'}>
+            Latest Blog Headline
+          </div>
           <div className={'latest-blog__button mb-3'}>
             <LandingButton color="yellow">Read more</LandingButton>
           </div>
