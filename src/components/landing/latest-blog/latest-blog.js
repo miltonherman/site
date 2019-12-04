b5f6d5ad60c8443dd41e4512b0eb00ce21952bcd
kukurudz-volodymyr <kukurudz.volodymyr@gmail.com>
2019-12-04 17:13:48 +0200

    focus and scale

--- src/components/landing/latest-blog/latest-blog.js
+++ src/components/landing/latest-blog/latest-blog.js
@@ -6,25 +6,27 @@ import '../landing.scss';
 class LatestBlog extends React.Component {
   render() {
     return (
-      <div className={'max-width-container center-content latest-blog'}>
-        <div className={'latest-blog__row my-4'}>
-          <div className={'latest-blog__title text-center landing__title mb-3'}>
-            Latest Roadmap Headline
+      <div className={'latest-blog-wrapper'}>
+        <div className={'max-width-container center-content latest-blog'}>
+          <div className={'latest-blog__row my-4'}>
+            <div className={'latest-blog__title text-center landing__title mb-3'}>
+              Latest Roadmap Headline
+            </div>
+            <div className={'landing__description text-center mb-3'}>
+              This is an impact statement. Something a little bit longer. Maybe mention the specific
+              pain point addressed.
+            </div>
+            <div className={'latest-blog__button'}>
+              <LandingButton color="yellow">Get the details</LandingButton>
+            </div>
           </div>
-          <div className={'landing__description text-center mb-3'}>
-            This is an impact statement. Something a little bit longer. Maybe mention the specific
-            pain point addressed.
-          </div>
-          <div className={'latest-blog__button'}>
-            <LandingButton color="yellow">Get the details</LandingButton>
-          </div>
-        </div>
-        <div className={'latest-blog__row my-4'}>
-          <div className={'latest-blog__title text-center landing__title mb-3'}>
-            Latest Blog Headline
-          </div>
-          <div className={'latest-blog__button mb-3'}>
-            <LandingButton color="yellow">Read more</LandingButton>
+          <div className={'latest-blog__row my-4'}>
+            <div className={'latest-blog__title text-center landing__title mb-3'}>
+              Latest Blog Headline
+            </div>
+            <div className={'latest-blog__button mb-3'}>
+              <LandingButton color="yellow">Read more</LandingButton>
+            </div>
           </div>
         </div>
       </div>
